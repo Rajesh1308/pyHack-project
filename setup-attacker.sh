548bb2cf0cba2b0b7611ea64d69c25ab6e8432ee
Rajesh1308 <rajesharulrathna@gmail.com>
2024-05-24 21:09:00 +0530

    Requirements installation

--- setup-attacker.sh
+++ setup-attacker.sh
@@ -11,7 +11,7 @@
 
 apt install python-is-python3 python3-pip iputils-ping net-tools -y
 apt install git -y
-pip install scapy
+pip install -r requirements.txt
 git clone https://github.com/Rajesh1308/pyHack-project.git
 cd pyHack-project/Network_scanner/
 echo "Enter the target ip/range : "
