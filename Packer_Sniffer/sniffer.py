46829bf751b103c6fc15bb17a4d73007ca84c072
Rajesh1308 <rajesharulrathna@gmail.com>
2024-06-08 14:27:37 +0530

    sniffer project started

---
+++ Packer_Sniffer/sniffer.py
@@ -0,0 +1,22 @@
+import scapy.all as scapy
+from scapy.layers import http
+import optparse
+
+def get_interface():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Network Interface to sniff packets")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify the interface, use --help for more information")
+    
+    return options.interface
+
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packets)
+
+def process_sniffed_packets(packet):
+    print(packet)
+
+get_interface()
+sniff()
